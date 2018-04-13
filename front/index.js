d87aceef6f796968df7e49877b4e05141e49311f
yanjingjie <yanjingjie86@126.com>
2018-04-13 16:03:12 +0800

    add test

--- front/index.js
+++ front/index.js
@@ -4,10 +4,6 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname,'public')));
 
-// app.get('/',(req,res) => {
-// 	res.render('index');
-// });
-
 app.listen(3001,err => {
 	if (err) {
 		throw new Error('程序出错了');
