02b012342c3164ef21e12aa7f172dc23ddf6f576
yanjingjie <yanjingjie86@126.com>
2018-04-13 15:57:18 +0800

    add Test

--- server/server.js
+++ server/server.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const cookie = require('cookie-parser');
 
-app.get('/ajax',(req,res) => {
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cookie());
+
+app.use("/",(req,res,next) => {
+	let reqUrl = req.headers.origin != null ? req.headers.origin : "*";
+	res.set({
+		'Access-Control-Allow-Origin':reqUrl,
+		'Access-Control-Allow-Methods':'*',
+		'Access-Control-Allow-Headers':'Content-Type',
+		'Access-Control-MaxAge':'3600',
+		'Access-Control-Allow-Credentials':'true',
+	});
+	next();
+})
+
+app.get('/ajax/getJson',(req,res) => {
 	let data = {"name":"yilule"};
-	/*res.append('Access-Control-Allow-Origin','http://localhost:3001');
-	res.append('Access-Control-Allow-Methods','GET');*/
-	res.append('Access-Control-Allow-Origin','*');
-	res.append('Access-Control-Allow-Methods','*');
 	res.jsonp(data);
 });
 
 app.post('/ajax/postJson',(req,res) => {
-	res.append('Access-Control-Allow-Origin','*');
-	res.append('Access-Control-Allow-Methods','*');
-	console.log(req.params);
-	res.jsonp(req.params)
-})
+	res.jsonp(req.body);
+});
+
+app.get('/ajax/getCookie',(req,res) => {
+	/*res.set({
+		'Set-Cookie':'user=yiluleCookie'
+	})*/
+	console.log(req.cookies);
+	res.jsonp(req.cookies);
+});
 
 app.listen(3000,err => {
 	if (err) {
